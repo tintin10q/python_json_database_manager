af50608913dd8d8f9ae42b180555747e076ef4cc
Quinten <tintin10q@hotmail.com>
2021-06-28 13:36:54 +0200

    Added backup functionality 🔙
    just call create_backup() and give a list of documents. Or all of them if you don't give anything or the string "all_of_them"

--- database_manager.py
+++ database_manager.py
@@ -1,167 +1,208 @@
 import copy
 import glob
 import json
+import time
+import shutil
 import os
 from multiprocessing import Lock
 
 from collections import UserDict
 
+
 class Database(UserDict):
-    """
-    This class is used to write and read stuff with a database made out of local json files
-    
-    The files are being read from disk and turned into dicts.
-    This class really shines with the context manager where you just ask for the name of a document and you
-    can edit it as a dict.
-    The dict is autometically written to disk when the context manager is closed.
-    
-    _save functions can be run on the class outside a context manager as they acquire the lock for the file.
-    functions without save are only meant to be run inside a with statement that will lock the file for them.
-    
-    When reading you can call the static methods. 
-    
-    Database.read(name)
-    
-    It is not recommended to use the write methods use the context manager instead.
-    When using the context manager make an instance of the class.
-    
-    with Database("document_name") as document:
-        document_name["new_data"] = new_data
-        
-    Document will be a dict so you can use document as if it is a dict!
-    
-    If anything goes wrong with the context manager makes a rollback to the state before any edits is made!
-    """
-
-    # generate locks
-    my_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(my_path)
-    locks = {file[:-5]: Lock() for file in glob.glob("*.json")}
-
-    def __init__(self, filename):
-        self.__name = filename
-        super().__init__()
-
-    @property
-    def lock(self):
-        """Returns appropriate lock for file with self.name."""
-        assert self.name in Database.locks, "You are trying to acces a database that does not exist."
-        return Database.locks[self.name]
-
-    @staticmethod
-    def get_lock(name):
-        """Returns lock based on input name."""
-        return Database.locks[name]
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        assert name in Database.locks, "You are trying to switch to a name that does not exist in the database."
-        self.__name = name
-
-    @staticmethod
-    def read(name):
-        """Will read <name>.json with a lock. Do not run in other lock that will cause deadlock"""
-        with Database.get_lock(name):
-            database = Database.__read(name)
-        return database
-
-    @staticmethod
-    def __read(name):
-        """Will read <name>.json without a lock. Maybe rename this to _read_unsafe?"""
-        database_path = os.path.join(os.path.dirname(__file__), name + ".json")
-        with open(database_path, "r+") as database_file:
-            database = json.load(database_file)
-        return database
-
-    def _readself(self):
-        """Will read <self.name>.json without a lock."""
-        return Database.__read(self.name)
-
-    @staticmethod
-    def write(name, data):
-        """Will write data to the <name>.json with a lock. Do not run in other lock that will cause deadlock. ALso do not run this in general use the context manager"""
-        with Database.get_lock(name):
-            return Database.__write(name, data)
-
-    @staticmethod
-    def __write(name, data):
-        """Will write data to <name>.json without a lock. Maybe rename this to _write_unsafe?"""
-        database_path = os.path.join(os.path.dirname(__file__), name + ".json")
-        with open(database_path, "w+") as file:
-            json.dump(data, file, indent=4, sort_keys=True)
-
-    def _writeself(self):
-        """Will write self:dict to <self.name>.json without a lock."""
-        Database.__write(self.name, self)
-
-    @staticmethod
-    def create(name, data, replace=False):
-        """Will create a new file named <name>.json with data inside and will add file to lock."""
-        if name not in Database.locks:
-            Database.locks[name] = Lock()
-            database_path = os.path.join(os.path.dirname(__file__), name + ".json")
-            open(database_path, "w+").close()
-            Database.write(name, data)
-            return data
-        elif name in Database.locks and replace:  # File already there
-            database_path = os.path.join(os.path.dirname(__file__), name + ".json")
-            open(database_path, "w+").close()
-            Database.write(name, data)
-            return data
-
-    @staticmethod
-    def add(name, data, key):
-        """ Will try to add the data under the identifier to the specified file. If the data is already in this file
-        it will override this data. This is a shorthand for combining get and set."""
-        with Database.get_lock(name):
-            database = Database.__read(name)
-            database[key] = data
-            Database.__write(name, database)
-
-    @staticmethod
-    def append(name, data):
-        """ Will append database to a list named <name> in a file named <name>.json."""
-        with Database.get_lock(name):
-            database = Database.__read(name)
-            database[name].append(data)
-            Database.__write(name, database)
-
-    @staticmethod
-    def reset_all(default_data):
-        """Will reset all databases. The reset state should be registered manually for special cases."""
-        for name, lock in Database.locks.items():
-            with lock:
-                Database.write(name, default_data)
-        return
-
-    @staticmethod
-    def translate(name, key):
-        """ Will translate a name to the corresponding ID """
-        if key in (translations := Database.read(name)):
-            return translations[key]
-        else:
-            return False
-
-    def __enter__(self):
-        self.clear()
-        self.lock.acquire()
-        self.data = self._readself()
-        self.backup_data = copy.deepcopy(self.data)  # Make a deep copy to preserve the backup and also lists inside the backup
-        self.update(self.data)  # Set dict data to self.data
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        try:
-            if exc_type is not None:  # There was an error in context attempt rollback and re-raise
-                self.__write(self.name, self.backup_data)
-            else:
-                try:  # Try to write new data
-                    self._writeself()
-                except TypeError as e:  # This can happen if data is not json serializable, Attempt rollback
-                    self.__write(self.name, self.backup_data)
-        finally:
-            Database.locks[self.name].release()
+	"""
+	This class is used to write and read stuff with a database made out of local json files
+	
+	The files are being read from disk and turned into dicts.
+	This class really shines with the context manager where you just ask for the name of a document and you
+	can edit it as a dict.
+	The dict is autometically written to disk when the context manager is closed.
+	
+	_save functions can be run on the class outside a context manager as they acquire the lock for the file.
+	functions without save are only meant to be run inside a with statement that will lock the file for them.
+	
+	When reading you can call the static methods.
+	
+	Database.read(name)
+	
+	It is not recommended to use the write methods use the context manager instead.
+	When using the context manager make an instance of the class.
+	
+	with Database("document_name") as document:
+		document_name["new_data"] = new_data
+		
+	Document will be a dict so you can use document as if it is a dict!
+	
+	If anything goes wrong with the context manager makes a rollback to the state before any edits is made!
+	
+	You can call create_backup(documents: list[str] = "all_of_them") to create a backup of documents
+	These will be put in a backup folder in a sub folder with a date. You can either give a list of document names
+	or just the string "all_of_them" which is the default for all documents to be backed up.
+	"""
+	
+	# generate locks
+	my_path = os.path.dirname(os.path.realpath(__file__))
+	backup_folder_path = os.path.join(my_path, "json-db-backups")
+	os.chdir(my_path)
+	locks = {file[:-5]: Lock() for file in glob.glob("*.json")}
+	
+	def __init__(self, filename):
+		self.__name = filename
+		super().__init__()
+	
+	@property
+	def lock(self):
+		"""Returns appropriate lock for file with self.name."""
+		assert self.name in Database.locks, "You are trying to acces a database that does not exist."
+		return Database.locks[self.name]
+	
+	@staticmethod
+	def get_lock(name):
+		"""Returns lock based on input name."""
+		return Database.locks[name]
+	
+	@property
+	def name(self):
+		return self.__name
+	
+	@name.setter
+	def name(self, name):
+		assert name in Database.locks, "You are trying to switch to a name that does not exist in the database."
+		self.__name = name
+	
+	@staticmethod
+	def read(name):
+		"""Will read <name>.json with a lock. Do not run in other lock that will cause deadlock"""
+		with Database.get_lock(name):
+			database = Database.__read(name)
+		return database
+	
+	@staticmethod
+	def __read(name):
+		"""Will read <name>.json without a lock. Maybe rename this to _read_unsafe?"""
+		database_path = os.path.join(os.path.dirname(__file__), name + ".json")
+		with open(database_path, "r+") as database_file:
+			database = json.load(database_file)
+		return database
+	
+	def _readself(self):
+		"""Will read <self.name>.json without a lock."""
+		return Database.__read(self.name)
+	
+	@staticmethod
+	def write(name, data):
+		"""Will write data to the <name>.json with a lock. Do not run in other lock that will cause deadlock. ALso do not run this in general use the context manager"""
+		with Database.get_lock(name):
+			return Database.__write(name, data)
+	
+	@staticmethod
+	def __write(name, data):
+		"""Will write data to <name>.json without a lock. Maybe rename this to _write_unsafe?"""
+		database_path = os.path.join(os.path.dirname(__file__), name + ".json")
+		with open(database_path, "w+") as file:
+			json.dump(data, file, indent=4, sort_keys=True)
+	
+	def _writeself(self):
+		"""Will write self:dict to <self.name>.json without a lock."""
+		Database.__write(self.name, self)
+	
+	@staticmethod
+	def create(name, data, replace=False):
+		"""Will create a new file named <name>.json with data inside and will add file to lock."""
+		if name not in Database.locks:
+			Database.locks[name] = Lock()
+			database_path = os.path.join(os.path.dirname(__file__), name + ".json")
+			open(database_path, "w+").close()
+			Database.write(name, data)
+			return data
+		elif name in Database.locks and replace:  # File already there
+			database_path = os.path.join(os.path.dirname(__file__), name + ".json")
+			open(database_path, "w+").close()
+			Database.write(name, data)
+			return data
+	
+	@staticmethod
+	def add(name, data, key):
+		""" Will try to add the data under the identifier to the specified file. If the data is already in this file
+		it will override this data. This is a shorthand for combining get and set."""
+		with Database.get_lock(name):
+			database = Database.__read(name)
+			database[key] = data
+			Database.__write(name, database)
+	
+	@staticmethod
+	def append(name, data):
+		""" Will append database to a list named <name> in a file named <name>.json."""
+		with Database.get_lock(name):
+			database = Database.__read(name)
+			database[name].append(data)
+			Database.__write(name, database)
+	
+	@staticmethod
+	def reset_all(default_data):
+		"""Will reset all databases. The reset state should be registered manually for special cases."""
+		for name, lock in Database.locks.items():
+			with lock:
+				Database.write(name, default_data)
+		return
+	
+	@staticmethod
+	def translate(name, key):
+		""" Will translate a name to the corresponding ID """
+		if key in (translations := Database.read(name)):
+			return translations[key]
+		else:
+			return False
+	
+	def __enter__(self):
+		self.clear()
+		self.lock.acquire()
+		self.data = self._readself()
+		self.backup_data = copy.deepcopy(self.data)  # Make a deep copy to preserve the backup and also lists inside the backup
+		self.update(self.data)  # Set dict data to self.data
+		return self
+	
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		try:
+			if exc_type is not None:  # There was an error in context attempt rollback and re-raise
+				self.__write(self.name, self.backup_data)
+			else:
+				try:  # Try to write new data
+					self._writeself()
+				except TypeError as e:  # This can happen if data is not json serializable, Attempt rollback
+					self.__write(self.name, self.backup_data)
+		finally:
+			Database.locks[self.name].release()
+	
+	@staticmethod
+	def create_backup(document_names: list[str] = "all_of_them"):
+		"""
+		Makes backups in a backup folder with the date
+		:param document_names:
+		:imports: datetime
+		"""
+		
+		if document_names == "all_of_them":
+			document_names = Database.locks.keys()
+		
+		if len(document_names) <= 0:
+			return
+		
+		locks = Database.locks
+		timestring = time.strftime("%Y%m%d-%H%M%S")
+		
+		if not os.path.exists(Database.backup_folder_path):
+			os.mkdir(Database.backup_folder_path)
+		
+		backup_path = os.path.join(Database.backup_folder_path, timestring)
+		
+		os.mkdir(backup_path)
+		
+		for document_name in document_names:
+			filename = document_name
+			if not filename.endswith(".json"):
+				filename += ".json"
+			src = os.path.join(Database.my_path, filename)
+			dst = os.path.join(backup_path, filename)
+			with locks[document_name]:
+				shutil.copy2(src, dst)
